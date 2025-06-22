773281141f91d2f19814afcfdf973279990b828f
Дарья Агафонова <daryia.agafonova@gmail.com>
2025-06-22 19:14:43 +0300

    01 - Решение: Задачник. Основы ч.1

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,20 @@
-import { defineComponent, createApp } from 'vue'
+import {defineComponent, createApp} from 'vue'
+
+const dateApp = createApp({
+  name: 'dateApp',
+  setup() {
+    function getFormatDate() {
+      const date = new Date();
+      let formatDate = date.toLocaleDateString("en-US", {dateStyle: 'long'});
+
+      return 'Сегодня ' + formatDate;
+    }
+
+    return {
+      getFormatDate,
+    }
+  },
+  'template': '<div>{{ getFormatDate() }}</div>'
+})
+
+const vm = dateApp.mount("#app")
